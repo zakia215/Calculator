a274582080840d8f00e2aa361ae6ce2a2d073298
zakia215 <zakiamanullah215@gmail.com>
2022-03-16 16:51:46 +0700

    Adding user input.

--- main.cpp
+++ main.cpp
@@ -6,5 +6,11 @@ using namespace std;
 int main() {
     double a, b;
     char op;
+    cout << "Please put in a number: ";
+    cin >> a;
+    cout << "Please put in an operator (+, -, /, *, ^)";
+    cin >> op;
+    cout << "Please put in another number: ";
+    cin >> b;
     return 0;
 }
